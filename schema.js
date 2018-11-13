20f10c6131bce2f3f4522ed7123e42128edbe6b3
Devin Herath <devdhera@gmail.com>
2018-11-13 18:48:38 +0530

    Configure endpoint to get an individual launch

--- schema.js
+++ schema.js
@@ -31,7 +31,19 @@ const RootQuery = new GraphQLObjectType({
         launches: {
             type: new GraphQLList(LaunchType),
             resolve(parent, args) {
-                return axios.get('https://api.spacexdata.com/v3/launches')
+                return axios
+                    .get('https://api.spacexdata.com/v3/launches')
+                    .then(res => res.data);
+            }
+        },
+        launch: {
+            type: LaunchType,
+            args: {
+                flight_number: { type: GraphQLInt }
+            },
+            resolve(parent, args) {
+                return axios
+                    .get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
                     .then(res => res.data);
             }
         }
