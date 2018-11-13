7ee68ea12ca1b747de56798a0be5f92efa5e3a82
Devin Herath <devdhera@gmail.com>
2018-11-13 18:59:04 +0530

    Fixed #2 by changing id type to a string

--- schema.js
+++ schema.js
@@ -58,11 +58,11 @@ const RootQuery = new GraphQLObjectType({
         rocket: {
             type: RocketType,
             args: {
-                id: { type: GraphQLInt }
+                rocket_id: { type: GraphQLString }
             },
             resolve(parent, args) {
                 return axios
-                    .get(`https://api.spacexdata.com/v3/rockets/${args.id}`)
+                    .get(`https://api.spacexdata.com/v3/rockets/${args.rocket_id}`)
                     .then(res => res.data);
             }
         }
